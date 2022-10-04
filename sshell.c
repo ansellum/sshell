e49b65d9419e627c1a03fed5191f5b792abc4cda
ansellum <jbvogt@ucdavis.edu>
2022-10-04 00:27:13 -0700

    Edited Makefile to allow for appending CFLAGS from command lind     Cleaned up phase 2 feature functions     Renamed variables & fixed child exit status

--- sshell.c
+++ sshell.c
@@ -6,49 +6,48 @@
 
 #define CMDLINE_MAX 512
 
-struct commandObj {
+typedef struct commandObj {
         char* program;
         char arguments[16][32];    //Array of 16 strings (0-15) that holds 32 characters each
-};
+}commandObj;
 
-void parseCommand(struct commandObj* obj, char *command) {
-        const char delimiter[2] = " ";
-        char* token;
+void parseCommand(struct commandObj* cmd, char *cmdString)
+{
         int index = 0;
-
         //get the first token and stuff it in the program property 
-        token = strtok(command, delimiter);
-        obj->program = token;
 
+        char *token = strtok(cmdString, " ");
+        cmd->program = token;
         //pass the arguments into the arguments array
         while (token != NULL) {
-                strcpy(obj->arguments[index], token);
-
                 //update token to next argument
-                token = strtok(NULL, delimiter);
+                token = strtok(NULL, " ");
+
+                strcpy(cmd->arguments[index], token);
 
                 index++;
+
         }
 }
 
  /* Executes an external command with fork(), exec(), & wait() (phase 1)*/
-void executeExternalProcess(char *cmd) 
+void executeExternalProcess(char *cmdString) 
 {
         int pid;
         int childStatus;
+        commandObj cmd;
 
         pid = fork();
         //child process should execute the command (takes no arguments yet)
         if (pid == 0) {
-                execlp(cmd, cmd, (char *) NULL);
+                parseCommand(&cmd, cmdString);
+                childStatus = execlp(cmdString, cmdString, (char *) NULL);
                 exit(1); //if child reaches this line it means there was an issue running command
         }
         //parent process should wait for child to execute
         else if (pid > 0) {
-                wait(&childStatus);
-                //print error status of child process to stderr
-                if (!WEXITSTATUS(childStatus)) fprintf(stderr, "+ completed '%s' [%d]\n",cmd, 0); //if child process is successful
-                else fprintf(stderr, "+ completed '%s' [%d]\n",cmd, 1);
+                waitpid(pid, &childStatus, 0);
+                printf("+ completed '%s' [%d]\n", cmdString, childStatus);
         }
         //handle error with fork()
         else fprintf(stderr, "error completing fork() [%d]\n", 1);
@@ -58,7 +57,7 @@ void executeExternalProcess(char *cmd)
 
 int main(void)
 {
-        char cmd[CMDLINE_MAX];
+        char cmdString[CMDLINE_MAX];
 
         while (1) {
                 char *nl;
@@ -68,25 +67,25 @@ int main(void)
                 fflush(stdout);
 
                 /* Get command line */
-                fgets(cmd, CMDLINE_MAX, stdin);
+                fgets(cmdString, CMDLINE_MAX, stdin);
 
                 /* Print command line if stdin is not provided by terminal */
                 if (!isatty(STDIN_FILENO)) {
-                        printf("%s", cmd);
+                        printf("%s", cmdString);
                         fflush(stdout);
                 }
 
                 /* Remove trailing newline from command line */
-                nl = strchr(cmd, '\n');
+                nl = strchr(cmdString, '\n');
                 if (nl) *nl = '\0';
 
                 /* Builtin command */
-                if (!strcmp(cmd, "exit")) {
+                if (!strcmp(cmdString, "exit")) {
                         fprintf(stderr, "Bye...\n");
-                        fprintf(stderr, "+ completed '%s' [%d]\n",cmd, 0);
+                        fprintf(stderr, "+ completed '%s' [%d]\n",cmdString, 0);
                         break;
                 }
-                else executeExternalProcess(cmd);
+                else executeExternalProcess(cmdString);
         }
         
         return EXIT_SUCCESS;
