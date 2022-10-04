02403bb58845ba486fb5600a62a605fa6a240355
ansellum <jbvogt@ucdavis.edu>
2022-10-04 11:40:02 -0700

    Integrated argument functionality (all programs with arguments should now work) cleaned up functions for readability

--- sshell.c
+++ sshell.c
@@ -5,29 +5,40 @@
 #include <unistd.h>
 
 #define CMDLINE_MAX 512
+#define NUMARGS_MAX 16
+#define ARGLENGTH_MAX 32
 
 typedef struct commandObj {
         char* program;
-        char arguments[16][32];    //Array of 16 strings (0-15) that holds 32 characters each
+        char* arguments[NUMARGS_MAX];
 }commandObj;
 
+/* Parses command into commandObj properties */
 void parseCommand(struct commandObj* cmd, char *cmdString)
 {
-        int index = 0;
+        char delim[] = " ";
+        char* token;
+
+        //make editable string from literal
+        char* buf = malloc(CMDLINE_MAX * sizeof(char));
+        strcpy(buf, cmdString); 
+
         //get the first token and stuff it in the program property 
-
-        char *token = strtok(cmdString, " ");
+        token = strtok(buf, delim);
         cmd->program = token;
-        //pass the arguments into the arguments array
+
+        //fill arguments array (program name is first in argument list; see execvp() man)
+        int i = 0;
         while (token != NULL) {
+                cmd->arguments[i] = malloc(ARGLENGTH_MAX * sizeof(char));
+                strcpy(cmd->arguments[i], token);
+
                 //update token to next argument
-                token = strtok(NULL, " ");
-
-                strcpy(cmd->arguments[index], token);
-
-                index++;
-
+                token = strtok(NULL, delim);
+                i++;
         }
+        //End arguments array with NULL for execvp() detection
+        cmd->arguments[i] = NULL;
 }
 
  /* Executes an external command with fork(), exec(), & wait() (phase 1)*/
@@ -36,12 +47,11 @@ void executeExternalProcess(char *cmdString)
         int pid;
         int childStatus;
         commandObj cmd;
-
+        parseCommand(&cmd, cmdString);
         pid = fork();
         //child process should execute the command (takes no arguments yet)
         if (pid == 0) {
-                parseCommand(&cmd, cmdString);
-                childStatus = execlp(cmdString, cmdString, (char *) NULL);
+                childStatus = execvp(cmd.program, cmd.arguments);
                 exit(1); //if child reaches this line it means there was an issue running command
         }
         //parent process should wait for child to execute
